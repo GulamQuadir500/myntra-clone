3731b284e91d5dede61da90dc3c99388609fcfb2
GulamQuadir500 <gulamquadir500@gmail.com>
2024-06-09 14:28:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// importing components
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductsListing from "./pages/ProductsListing";
+import SeparateProduct from "./pages/SeparateProduct";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Bag from "./pages/Bag";
+import Wishlist from "./pages/Wishlist";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import PrivateRoute from "./components/PrivateRoute";
+import AuthPrivateRoute from "./components/AuthPrivate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        {/* routes for navigating */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsListing />} />
+        <Route path="/product/:id" element={<SeparateProduct />} />
+        <Route element={<AuthPrivateRoute />}>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/bag" element={<Bag />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// Importing Provider from react-redux for Redux store integration
+import { Provider } from "react-redux";
+// Importing the Redux store and persistor
+import { store, persistor } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Importing PersistGate for Redux persist integration
+import { PersistGate } from "redux-persist/integration/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrapping the entire application inside React.StrictMode for additional development mode checks */}
+
+    <Provider store={store}>
+      {/* Providing the Redux store to the application */}
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
